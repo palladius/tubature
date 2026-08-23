56848bfb9b526df5f214c05c7d0fbec2a6ec1eba
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-08-23 08:51:56 +0200

    deploy: v2.0.1 — smooth curved pipes + harder difficulty 🎨

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2c8326970fbf8c4af76c4d7295420ef9",
-"version.json": "7e020c420352d97e77f8fecadb10176f",
+const RESOURCES = {"flutter_bootstrap.js": "314fbd118fea889475dc5b3fa8c6d9e5",
+"version.json": "2634e402de7d976bd6a28d809b5e4232",
 "index.html": "6b19813b831f8c38bc0e6fca790a33a2",
 "/": "6b19813b831f8c38bc0e6fca790a33a2",
-"main.dart.js": "a76c29f0c344a71705661bcdd5aee8a8",
+"main.dart.js": "d7f2a97effc1385fd0640cbdc9d704a4",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "2c8326970fbf8c4af76c4d7295420ef9",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "b5840701612184b91f27cafd9e56d486",
+"assets/fonts/MaterialIcons-Regular.otf": "2bc7d4fe88455477a6120e6a1361e405",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
